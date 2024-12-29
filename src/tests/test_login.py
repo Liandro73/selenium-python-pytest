be7e76e2d4ad1c768b8f3a4b9fa2dcb7796eafc5
Dernival Liandro <liandro.sys@gmail.com>
2024-12-29 11:58:25 +0000

    Running tests on chrome and firefox

--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -1,14 +1,15 @@
 import allure
+import pytest
 
 from src.pages.login_page import LoginPage as loginPage
 from src.pages.products_page import ProductsPage as productsPage
-from src.tests.conftest import driver
+from src.tests.conftest import driver, browser
 
 @allure.suite("Login")
 @allure.title("Login successfully")
 @allure.description("This is a test about logging in Swag Labs - Successfully")
 def test_login_successfully(driver, config, browser):
-    print(browser)
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "standard_user")
     loginPage.fill_password_field(driver, "secret_sauce")
@@ -18,7 +19,8 @@ def test_login_successfully(driver, config, browser):
 @allure.suite("Login")
 @allure.title("Login with empty username")
 @allure.description("This is a test about logging in Swag Labs - Empty username")
-def test_login_empty_username(driver, config):
+def test_login_empty_username(driver, config, browser):
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "")
     loginPage.fill_password_field(driver, "secret_sauce")
@@ -28,7 +30,8 @@ def test_login_empty_username(driver, config):
 @allure.suite("Login")
 @allure.title("Login with empty password")
 @allure.description("This is a test about logging in Swag Labs - Empty password")
-def test_login_empty_password(driver, config):
+def test_login_empty_password(driver, config, browser):
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "standard_user")
     loginPage.fill_password_field(driver, "")
@@ -38,7 +41,8 @@ def test_login_empty_password(driver, config):
 @allure.suite("Login")
 @allure.title("Login with user locked out")
 @allure.description("This is a test about logging in Swag Labs - User locked out")
-def test_login_user_locked_out(driver, config):
+def test_login_user_locked_out(driver, config, browser):
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "locked_out_user")
     loginPage.fill_password_field(driver, "secret_sauce")
@@ -48,7 +52,8 @@ def test_login_user_locked_out(driver, config):
 @allure.suite("Login")
 @allure.title("Login with user invalid")
 @allure.description("This is a test about logging in Swag Labs - User invalid")
-def test_login_user_invalid(driver, config):
+def test_login_user_invalid(driver, config, browser):
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "standard_user123")
     loginPage.fill_password_field(driver, "secret_sauce")
@@ -58,7 +63,8 @@ def test_login_user_invalid(driver, config):
 @allure.suite("Login")
 @allure.title("Login with password invalid")
 @allure.description("This is a test about logging in Swag Labs - Password invalid")
-def test_login_password_invalid(driver, config):
+def test_login_password_invalid(driver, config, browser):
+    allure.dynamic.tag("Login", browser)
     loginPage.verify_if_is_login_page(driver, config)
     loginPage.fill_username_field(driver, "standard_user123")
     loginPage.fill_password_field(driver, "secret_sauce123")
